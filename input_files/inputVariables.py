5d49e20b216774265cbcea2557ee7d77feaf7e49
nero-tyagi <nerotgi@gmail.com>
2025-10-20 23:38:51 -0400

    Added inputVariables.py file

---
+++ input_files/inputVariables.py
@@ -0,0 +1,13 @@
+MPH_FILES = [
+    # 'input_files/SDPR2.mph',
+    # 'input_files/SDPR3-1.mph',
+    # 'input_files/SDPR3-2.mph',
+    # 'input_files/SDPR3-3.mph',
+    'input_files/SDPR3-4.mph',
+    # 'input_files/SDPR3-5.mph',
+    # 'input_files/SDPR3-6.mph',
+    # 'input_files/SDPR4.mph',
+]
+
+EXPORT_PLOT_NODES = [EXPORT_DICT[1], EXPORT_DICT[2], EXPORT_DICT[3],
+                     EXPORT_DICT[4], EXPORT_DICT[5], EXPORT_DICT[6]]
