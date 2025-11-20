6c5edf8ce073d0c49d057de546d26daf095f8b88
nero-tyagi <nerotgi@gmail.com>
2025-11-19 19:46:56 -0500

    Finalized calc_mdot function and moved it to collectionScatter.py

--- controllers/testController.py
+++ controllers/testController.py
@@ -1,21 +1,5 @@
 import numpy as np
 import pandas as pd
-
-column_names = ['r', 'Ws', 'Hs', 'theta', 'phid', 'udz']
-df = pd.read_table("data/outlet.txt", sep='\t', skiprows=9 , header=None,
-                   names=column_names, dtype=float)
-
-mdot = np.zeros(len(df['r']))
-
-for i in range(2, len(df['r'])):
-    mdot[i] = (2 * np.pi *
-               ((df['r'].iloc[i])) / 100 *
-               ((df['r'].iloc[i]) - (df['r'].iloc[i-1])) / 100 *
-               (-1 * df['udz'].iloc[i]) *
-               1200 *
-               df['phid'].iloc[i] / 1E6)
-
-total_flow_rate = sum(mdot)
-print(total_flow_rate)
+from functions.collectionScatter import create_mass_flow_scatter, calc_mdot
 
 create_mass_flow_scatter()
--- functions/collectionScatter.py
+++ functions/collectionScatter.py
@@ -52,3 +52,21 @@ def create_mass_flow_scatter(filename_prefix = filename_prefix):
     fig.write_image(directory + 'scatter/' + x_var + '.png', format='png', scale=1, width=1000, height=1000)
     print("Scatter plot files written.")
     # fig.show()
+
+def calc_mdot():
+    column_names = ['r', 'Ws', 'Hs', 'theta', 'phid', 'udz']
+    df = pd.read_table("data/outlet.txt", sep='\t', skiprows=9, header=None,
+                       names=column_names, dtype=float)
+
+    mdot = np.zeros(len(df['r']))
+
+    for i in range(2, len(df['r'])):
+        mdot[i] = (2 * np.pi *
+                   ((df['r'].iloc[i])) / 100 *
+                   ((df['r'].iloc[i]) - (df['r'].iloc[i - 1])) / 100 *
+                   (-1 * df['udz'].iloc[i]) *
+                   1200 *
+                   df['phid'].iloc[i] / 1E6)
+
+    total_flow_rate = sum(mdot)
+    return total_flow_rate
